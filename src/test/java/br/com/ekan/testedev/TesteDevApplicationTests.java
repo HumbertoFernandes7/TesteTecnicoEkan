23c3a2dc3541f3698abf6508bc4d94c2fb4543e0
Humberto Fernandes <humbertofernandes08@hotmail.com>
2025-10-15 20:37:56 -0300

    Iniciando aplicação

---
+++ src/test/java/br/com/ekan/testedev/TesteDevApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ekan.testedev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
