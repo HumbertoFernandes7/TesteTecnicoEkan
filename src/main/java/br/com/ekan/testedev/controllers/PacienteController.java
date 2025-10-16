19bdf8b190331efebb2973462eb204de7130a5f1
Humberto Fernandes <humbertofernandes08@hotmail.com>
2025-10-15 22:07:46 -0300

    Finalizando CRUD de Paciente

--- src/main/java/br/com/ekan/testedev/controllers/PacienteController.java
+++ src/main/java/br/com/ekan/testedev/controllers/PacienteController.java
@@ -10,10 +10,9 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -28,5 +27,31 @@ public class PacienteController {
         PacienteEntity pacienteEntity = pacienteMapper.inputToEntity(pacienteInput);
         PacienteEntity pacienteSalvo = pacienteService.create(pacienteEntity);
         return ResponseEntity.status(HttpStatus.CREATED).body(pacienteMapper.entityToOutput(pacienteSalvo));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PacienteOutput> findById(@PathVariable Long id) {
+        PacienteEntity pacienteEntity = pacienteService.findById(id);
+        return ResponseEntity.ok(pacienteMapper.entityToOutput(pacienteEntity));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PacienteOutput>> findAll() {
+        List<PacienteEntity> pacientes = pacienteService.findAll();
+        return ResponseEntity.ok().body(pacienteMapper.listEntityToListOutput(pacientes));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PacienteOutput> update(@RequestBody @Valid PacienteInput pacienteInput, @PathVariable Long id) {
+        PacienteEntity pacienteEncontrado = pacienteService.findById(id);
+        pacienteMapper.copyToEntity(pacienteInput, pacienteEncontrado);
+        PacienteEntity pacienteAtualizado = pacienteService.update(pacienteEncontrado);
+        return ResponseEntity.ok().body(pacienteMapper.entityToOutput(pacienteAtualizado));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        pacienteService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/br/com/ekan/testedev/mappers/PacienteMapper.java
+++ src/main/java/br/com/ekan/testedev/mappers/PacienteMapper.java
@@ -5,6 +5,9 @@ import br.com.ekan.testedev.dtos.inputs.PacienteOutput;
 import br.com.ekan.testedev.entities.PacienteEntity;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Component
 public class PacienteMapper {
 
@@ -19,5 +22,15 @@ public class PacienteMapper {
         pacienteOutput.setId(pacienteEntity.getId());
         pacienteOutput.setNome(pacienteEntity.getNome());
         return pacienteOutput;
+    }
+
+    public List<PacienteOutput> listEntityToListOutput(List<PacienteEntity> pacientesEntity){
+        return pacientesEntity.stream()
+                .map(this::entityToOutput)
+                .collect(Collectors.toList());
+    }
+
+    public void copyToEntity(PacienteInput pacienteInput, PacienteEntity pacienteEncontrado) {
+        pacienteEncontrado.setNome(pacienteInput.getNome());
     }
 }
